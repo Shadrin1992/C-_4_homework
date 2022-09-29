692db5b9bdd62fb39ad1b7a0c0c13613ebadef73
Evgeny <shadrin_pharm@mail.ru>
2022-09-29 23:22:42 +0300

    Решили вторую задачу

---
+++ task_27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+string num1 = Convert.ToString(Console.ReadLine());
+
+int[] arr = new int[num1.Length];
+int i;
+int result = 0;
+
+for(i = 0; i < num1.Length; i++)
+{
+    arr[i] = Convert.ToInt32(num1[i].ToString());
+}
+
+for(i=0; i < num1.Length; i++)
+{
+    result = result + arr[i];
+}
+Console.WriteLine(result);
